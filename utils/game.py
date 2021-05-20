891231911c18955e19349db52c1c7fc66b3e3760
hakan ergin <hakan.ergin@gmail.com>
2021-05-20 11:33:53 +0200

    initial commit files and class created

---
+++ utils/game.py
@@ -0,0 +1,26 @@
+
+class Hangman:
+    '''the game'''
+    def __init__(self):
+        '''the game constructor'''
+        self.possible_words = ['becode', 'learning', 'mathematics', 'sessions']
+        self.word_to_find = []
+        self.lives = 5
+        self.correctly_guessed_letters = []
+        self.wrongly_guessed_letters = []
+        self.turn_count = int
+        self.error_count = int
+
+    def play():
+        print("please enter a letter")
+        letter = input()
+        
+    def start_game():
+        pass
+      
+    def game_over():
+        pass
+
+    def well_played():
+        pass
+
